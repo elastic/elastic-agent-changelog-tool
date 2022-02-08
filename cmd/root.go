802072298cf03704099afe4d2ef4f01f4e7f84e1
LucianPy <lucian.pyc@gmail.com>
2022-02-08 12:32:03 +0200

    Add RootCmd command

---
+++ cmd/root.go
@@ -0,0 +1,22 @@
+// Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+// or more contributor license agreements. Licensed under the Elastic License
+// 2.0; you may not use this file except in compliance with the Elastic License
+// 2.0.
+
+package cmd
+
+import (
+	"github.com/spf13/cobra"
+)
+
+// RootCmd creates and returns root cmd for elastic-agent-changelog.
+func RootCmd() *cobra.Command {
+
+	rootCmd := &cobra.Command{
+		Use:          "elastic-agent-changelog",
+		Short:        "elastic-agent-changelog - Command line tool used for managing the change log for Elastic Agent and related components, including Beats.",
+		SilenceUsage: true,
+	}
+
+	return rootCmd
+}
