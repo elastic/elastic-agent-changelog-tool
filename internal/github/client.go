e3cad6d1225446bfab553a064eb369bb78ab70ac
Edoardo Tenani <edoardo.tenani@pm.me>
2022-04-12 17:25:57 +0200

    github: extract interfaces

--- internal/github/client.go
+++ internal/github/client.go
@@ -1,53 +1,21 @@
-// Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
-// or more contributor license agreements. Licensed under the Elastic License 2.0;
-// you may not use this file except in compliance with the Elastic License 2.0.
-
 package github
 
 import (
-	"context"
-	"fmt"
 	"net/http"
 
-	"github.com/google/go-github/v32/github"
+	gh "github.com/google/go-github/v32/github"
 )
 
-type GithubClient struct {
-	client Client
+type Client struct {
+	PullRequests githubPullRequestsService
+	Users        githubUsersService
 }
 
-func NewClient(gclient Client) (*GithubClient, error) {
-	return &GithubClient{
-		client: gclient,
-	}, nil
-}
+func NewClient(httpClient *http.Client) *Client {
+	client := gh.NewClient(httpClient)
 
-func NewUnauthorizedClient() *GithubClient {
-	return &GithubClient{
-		client: NewWrapper(github.NewClient(new(http.Client))),
+	return &Client{
+		PullRequests: client.PullRequests,
+		Users:        client.Users,
 	}
-}
-
-func (c *GithubClient) User() (string, error) {
-	user, _, err := c.client.UsersGet(context.Background(), "")
-	if err != nil {
-		return "", fmt.Errorf("fetching authenticated user failed: %w", err)
-	}
-
-	return *user.Login, nil
-}
-
-func (c *GithubClient) ListPullRequestsWithCommit(
-	ctx context.Context,
-	owner string,
-	repo string,
-	sha string,
-	opts *github.PullRequestListOptions,
-) ([]*github.PullRequest, *github.Response, error) {
-	pr, resp, err := c.client.ListPullRequestsWithCommit(ctx, owner, repo, sha, opts)
-	if err != nil {
-		return nil, nil, fmt.Errorf("failed to list pull requests with commit: %w", err)
-	}
-
-	return pr, resp, nil
 }
