5268f3a0bea3890b93887a2abf31dcaf154a7f43
Edoardo Tenani <edoardo.tenani@pm.me>
2022-04-27 12:07:03 +0200

    github: add better test cases for find-pr

--- internal/github/find-pr_test.go
+++ internal/github/find-pr_test.go
@@ -19,11 +19,49 @@ func TestFindPR(t *testing.T) {
 	c := github.NewClient(hc)
 	ctx := context.Background()
 
-	// https://github.com/elastic/elastic-agent-changelog-tool/commit/9c995a2e397d346e68ea5052c54bcbd0f8b142ad
-	res, err := github.FindPR(ctx, c, "elastic", "elastic-agent-changelog-tool", "9c995a2e397d346e68ea5052c54bcbd0f8b142ad")
+	// This is a PR merged on trunk:
+	// https://github.com/elastic/beats/pull/30859
+	// https://github.com/elastic/beats/commit/56df883ca93b11816206dad401c49a2c96fa268d
+	res, err := github.FindPR(ctx, c, "elastic", "beats", "56df883ca93b11816206dad401c49a2c96fa268d")
 	require.NoError(t, err)
 	require.Len(t, res.Items, 1)
-	require.Equal(t, res.Items[0].PullRequestID, 30)
+	require.Equal(t, 30859, res.Items[0].PullRequestID)
+}
+
+func TestFindPR_backport(t *testing.T) {
+	r, hc := getHttpClient(t)
+	defer r.Stop() //nolint:errcheck
+
+	c := github.NewClient(hc)
+	ctx := context.Background()
+
+	// This is a merge commit of a backport from main to 8.1:
+	// from https://github.com/elastic/beats/pull/31279 to https://github.com/elastic/beats/pull/31343
+	// https://github.com/elastic/beats/commit/fe25c73907336fc462d5e6e059d3cd86512484fe
+	res, err := github.FindPR(ctx, c, "elastic", "beats", "fe25c73907336fc462d5e6e059d3cd86512484fe")
+	require.NoError(t, err)
+	require.Len(t, res.Items, 4)
+	require.Equal(t, 31396, res.Items[0].PullRequestID)
+	require.Equal(t, 31417, res.Items[1].PullRequestID)
+	require.Equal(t, 31382, res.Items[2].PullRequestID)
+	require.Equal(t, 31343, res.Items[3].PullRequestID)
+}
+
+func TestFindPR_forwardport(t *testing.T) {
+	t.Skip("this behaviour is not yet implemented")
+	r, hc := getHttpClient(t)
+	defer r.Stop() //nolint:errcheck
+
+	c := github.NewClient(hc)
+	ctx := context.Background()
+
+	// This is a merge commit of a forwardport from 8.0 to main:
+	// from https://github.com/elastic/beats/issues/29209 to https://github.com/elastic/beats/pull/30626
+	// https://github.com/elastic/beats/commit/8800e5f6ad5beb024dee141a2639630b79a99a37
+	res, err := github.FindPR(ctx, c, "elastic", "beats", "8800e5f6ad5beb024dee141a2639630b79a99a37")
+	require.NoError(t, err)
+	require.Len(t, res.Items, 1)
+	require.Equal(t, 29209, res.Items[0].PullRequestID)
 }
 
 func TestFindPR_missingCommit(t *testing.T) {
