3a97334ee0d455f2022ff194f3570707bb85e145
Shaunak Kashyap <ycombinator@gmail.com>
2023-06-22 05:35:14 -0700

    [`new` sub-command] Default title to git branch name (#142)
    * Default title to git branch name
    
    * Fix default in flag
    
    * Get title from positional args

--- cmd/new.go
+++ cmd/new.go
@@ -5,15 +5,14 @@
 package cmd
 
 import (
-	"errors"
-
+	"fmt"
 	"github.com/elastic/elastic-agent-changelog-tool/internal/changelog/fragment"
 	"github.com/spf13/afero"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
+	"os/exec"
+	"strings"
 )
-
-var errNewCmdMissingArg = errors.New("new requires title argument")
 
 func NewCmd() *cobra.Command {
 
@@ -21,13 +20,25 @@ func NewCmd() *cobra.Command {
 		Use:   "new title",
 		Short: "Create a new changelog fragment",
 		Args: func(cmd *cobra.Command, args []string) error {
-			if len(args) < 1 {
-				return errNewCmdMissingArg
-			}
 			return nil
 		},
 		RunE: func(cmd *cobra.Command, args []string) error {
-			title := args[0]
+			// Determine changelog fragment title
+			var title string
+			if len(args) > 0 {
+				// Title is the string that follows "elastic-agent-changelog-tool new"
+				title = strings.Join(args, " ")
+			} else {
+				// Title is the name of the git branch
+				var err error
+				title, err = gitBranchName()
+				if err != nil {
+					return fmt.Errorf("error setting default title: %w", err)
+				}
+			}
+
+			title = strings.ToLower(strings.TrimSpace(title))
+
 			location := viper.GetString("fragment_location")
 			fc := fragment.NewCreator(afero.NewOsFs(), location)
 
@@ -39,5 +50,18 @@ func NewCmd() *cobra.Command {
 		},
 	}
 
+	newCmd.Flags().String("title", "", "The title for the changelog")
+
 	return newCmd
+}
+
+func gitBranchName() (string, error) {
+	// git symbolic-ref --short HEAD
+	cmd := exec.Command("git", "symbolic-ref", "--short", "HEAD")
+	stdout, err := cmd.Output()
+	if err != nil {
+		return "", fmt.Errorf("unable to determine git branch: %w", err)
+	}
+
+	return string(stdout), nil
 }
