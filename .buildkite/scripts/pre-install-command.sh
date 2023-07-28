5de1045a831a18c82c33d913d3b9226fd4e48831
Svetlana <5437289+leo-ri@users.noreply.github.com>
2023-07-28 20:16:12 +0200

    debug

--- .buildkite/scripts/pre-install-command.sh
+++ .buildkite/scripts/pre-install-command.sh
@@ -20,9 +20,9 @@ with_go() {
     go_version=$1
     url=$(get_gvm_link "${GVM_VERSION}")
     retry 5 curl -sL -o "${WORKSPACE}/bin/gvm" "${url}"
-    ls ${WORKSPACE}/bin/ -l
     chmod +x "${WORKSPACE}/bin/gvm"
     ls ${WORKSPACE}/bin/ -l
+    which gvm
     eval "$(gvm $go_version)"
     go version
 }
@@ -33,11 +33,10 @@ get_gvm_link() {
     platform_type="$(uname)"
     platform_type_lowercase="${platform_type,,}"
     arch_type="$(uname -m)"
-    [[ ${arch_type} == "aarch64" ]] && arch_type="amd64" # gvm do not have 'aarch64' name for archetecture type
+    [[ ${arch_type} == "aarch64" ]] && arch_type="arm64" # gvm do not have 'aarch64' name for archetecture type
     echo "https://github.com/andrewkroh/gvm/releases/download/${gvm_version}/gvm-${platform_type_lowercase}-${arch_type}"
 }
 
 # Required env variables:
 #   WORKSPACE
-#   SETUP_MAGE_VERSION
 WORKSPACE=${WORKSPACE:-"$(pwd)"}
