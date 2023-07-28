59bc4d59b0574c1950ae87cd427341255bce8e2e
Svetlana <5437289+leo-ri@users.noreply.github.com>
2023-07-28 19:26:08 +0200

    gcp agent

--- .buildkite/scripts/pre-install-command.sh
+++ .buildkite/scripts/pre-install-command.sh
@@ -16,6 +16,25 @@ with_goreleaser() {
     go install github.com/goreleaser/goreleaser@v1.6.3
 }
 
+with_go() {
+    go_version=$1
+    url=$(get_gvm_link "${GVM_VERSION}")
+    retry 5 curl -sL -o "${WORKSPACE}/bin/gvm" "${url}"
+    chmod +x "${WORKSPACE}/bin/gvm"
+    eval "$(gvm $go_version)"
+    go version
+}
+
+# for gvm link
+get_gvm_link() {
+    gvm_version=$1
+    platform_type="$(uname)"
+    platform_type_lowercase="${platform_type,,}"
+    arch_type="$(uname -m)"
+    [[ ${arch_type} == "aarch64" ]] && arch_type="amd64" # gvm do not have 'aarch64' name for archetecture type
+    echo "https://github.com/andrewkroh/gvm/releases/download/${gvm_version}/gvm-${platform_type_lowercase}-${arch_type}"
+}
+
 # Required env variables:
 #   WORKSPACE
 #   SETUP_MAGE_VERSION
--- .buildkite/scripts/snapshot.sh
+++ .buildkite/scripts/snapshot.sh
@@ -5,6 +5,7 @@ set -euo pipefail
 echo "--- Pre install"
 source .buildkite/scripts/pre-install-command.sh
 add_bin_path
+with_go "${GO_VERSION}"
 with_goreleaser
 
 echo "--- Snapshot"
