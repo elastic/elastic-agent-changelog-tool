7c606b76b687f1dc77185c514287a8e4ef8852c6
Edoardo Tenani <edoardo.tenani@elastic.co>
2023-09-22 16:26:18 +0200

    remove ' from fragment filenames

--- internal/changelog/fragment/creator.go
+++ internal/changelog/fragment/creator.go
@@ -84,5 +84,6 @@ func sanitizeFilename(s string) string {
 	s = strings.Replace(s, "/", "-", -1)
 	s = strings.Replace(s, "\\", "-", -1)
 	s = strings.Replace(s, ":", "", -1)
+	s = strings.Replace(s, "'", "", -1)
 	return s
 }
